ad1b38c70d6763a04cd52beda7cfcbf10471c8f5
Luis Renteria <lrenteria1604@gmail.com>
2026-04-28 15:37:30 -0600

    Trust proxies for HTTPS on Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
 
     ->withExceptions(function (Exceptions $exceptions): void {
